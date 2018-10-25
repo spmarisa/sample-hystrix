3371981a912ac2d45b80c4f4e8cd99960f1b4695
Phaneendra Marisa <phaneendra.marisa@paysafe.com>
2018-10-25 18:51:53 +0530

    initial commit

---
+++ src/main/java/xyz/juxtapose/samplehystrix/SampleHystrixApplication.java
@@ -0,0 +1,12 @@
+package xyz.juxtapose.samplehystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHystrixApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/juxtapose/samplehystrix/SampleHystrixApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.juxtapose.samplehystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleHystrixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
